1310f602fb2b221e6e5a69f0c0564a094f54d531
Marina Flores <mflores@ionic.health>
2023-07-27 08:01:09 -0300

    feat: crud de classes

--- src/main/java/com/challenge2/challenge2/Challenge2Application.java
+++ src/main/java/com/challenge2/challenge2/Challenge2Application.java
@@ -11,8 +11,5 @@ public class Challenge2Application {
 	public static void main(String[] args) {
 
         SpringApplication.run(Challenge2Application.class, args);
-
-
 	}
-
 }
--- src/main/java/com/challenge2/challenge2/restControllers/ClassController.java
+++ src/main/java/com/challenge2/challenge2/restControllers/ClassController.java
@@ -1,8 +1,49 @@
 package com.challenge2.challenge2.restControllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.challenge2.challenge2.entities.Classes;
+import com.challenge2.challenge2.services.impl.ClassServiceImpl;
+
 @RestController
+@RequestMapping("/api/classes")
 public class ClassController {
 
+    private ClassServiceImpl classService;
+
+    public ClassController(ClassServiceImpl classService){
+        this.classService = classService;
+    }
+
+    @GetMapping
+    public List<Classes> getAllClasses(){
+        return classService.getAllClasses();
+    }
+
+    @GetMapping("/{id}")
+    public Classes getClassById(@PathVariable Long id){
+        return classService.getClassById(id);
+    }
+    
+    @PostMapping
+    public Classes addClass(@RequestBody Classes classes){
+        return classService.saveClass(classes);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteClass(@PathVariable Long id){
+       Classes c = classService.getClassById(id);
+
+       if(c != null){
+        classService.deleteClass(c.getId());
+       }
+    }
 }
