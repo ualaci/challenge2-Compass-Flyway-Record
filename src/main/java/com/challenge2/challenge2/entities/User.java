1d38917b44dde6cefdbf038b14b4edcc86aaa29a
Ualaci Anjos <ualaci.anjos.pb@compasso.com.br>
2023-07-27 22:43:08 -0300

    Atualizacao de Validacoes, modificacao do relacionamento do squad

--- src/main/java/com/challenge2/challenge2/entities/User.java
+++ src/main/java/com/challenge2/challenge2/entities/User.java
@@ -1,6 +1,9 @@
 package com.challenge2.challenge2.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Data
@@ -14,19 +17,23 @@ import lombok.*;
 public class User{
 
     @Id
-    @Column(name = "user_id")
+    @Column(name = "userId")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long Id;
 
     @Column(name = "name")
     private String name;
 
+    @Email
     @Column(name = "email")
     private String email;
 
     @Column(name = "city")
     private String city;
 
+    @Size(min = 8, max = 20, message = "A senha deve ter entre 8 e 20 caracteres")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",
+    message = "A senha deve conter pelo menos um número, uma letra maiúscula, uma letra minúscula, um caractere especial e não pode conter espaços em branco.")
     @Column(name = "password")
     private String password;
 
