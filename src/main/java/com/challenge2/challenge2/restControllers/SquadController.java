ed525971bf4c64ee80ca9a02b996447252082944
Ualaci Anjos <ualaci.anjos.pb@compasso.com.br>
2023-07-28 15:47:08 -0300

    commit de alterações do squad

--- src/main/java/com/challenge2/challenge2/restControllers/SquadController.java
+++ src/main/java/com/challenge2/challenge2/restControllers/SquadController.java
@@ -1,7 +1,81 @@
 package com.challenge2.challenge2.restControllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.challenge2.challenge2.entities.Squad;
+import com.challenge2.challenge2.entities.Student;
+import com.challenge2.challenge2.services.impl.SquadServiceImpl;
+import com.challenge2.challenge2.services.impl.StudentServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/squad")
 public class SquadController {
+
+    private final SquadServiceImpl squadService;
+    private final StudentServiceImpl studentService;
+
+    @Autowired
+    public SquadController (SquadServiceImpl squadService, StudentServiceImpl studentService){
+        this.squadService = squadService;
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Squad> getAllSquads() {
+        return squadService.getAllSquads();
+
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Squad> getSquadById(@PathVariable Long id) {
+
+        return squadService.getSquadById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity addSquad(@RequestBody Squad squad) {
+        Squad savedSquad = squadService.saveSquad(squad);
+        return new ResponseEntity(savedSquad, HttpStatus.CREATED);
+    }
+
+
+    @PostMapping("/perID")
+    public ResponseEntity<String> createSquadWithStudents(@RequestBody Squad squad) {
+        for (Student student : squad.getStudents()) {
+            if (!studentService.existsById(student.getId())) {
+                Long studentID = student.getId();
+                String message = "O aluno com ID " + studentID + " não foi encontrado.";
+                return ResponseEntity.badRequest().body(message);
+            }
+        }
+        Squad createdSquad = squadService.createSquad(squad);
+
+        return new ResponseEntity(createdSquad, HttpStatus.CREATED);
+    }
+
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteSquad(@PathVariable Long id){
+        return squadService.getSquadById(id).map(entidade ->{
+            squadService.deleteSquad(entidade.getSquadId());
+            return new ResponseEntity( HttpStatus.NO_CONTENT);
+        }).orElseGet(() ->
+                new ResponseEntity("Squad não encontrada na base de dados", HttpStatus.BAD_REQUEST));
+
+    }
+    @PutMapping
+    public ResponseEntity<String> updateSquad(@RequestBody Squad squad){
+        return squadService.getSquadById(squad.getSquadId()).map(entidade -> {
+            squadService.saveSquad(squad);
+            return new ResponseEntity<String>("Squad atualizada com sucesso!", HttpStatus.OK);
+        }).orElseGet(() ->
+                new ResponseEntity<String>("Essa squad não existe!", HttpStatus.BAD_REQUEST));
+    }
+
 }
--- src/main/java/com/challenge2/challenge2/services/impl/SquadServiceImpl.java
+++ src/main/java/com/challenge2/challenge2/services/impl/SquadServiceImpl.java
@@ -1,35 +1,90 @@
 package com.challenge2.challenge2.services.impl;
 
 import com.challenge2.challenge2.entities.Squad;
+import com.challenge2.challenge2.entities.Student;
 import com.challenge2.challenge2.repositories.SquadRepository;
+import com.challenge2.challenge2.repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SquadServiceImpl implements SquadService{
 
 
-    private SquadRepository squadRepository;
+    private static SquadRepository squadRepository;
+    private static StudentRepository studentRepository;
 
-
-    public SquadServiceImpl(SquadRepository squadRepository) {
-
+    @Autowired
+    public SquadServiceImpl(SquadRepository squadRepository, StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
         this.squadRepository = squadRepository;
     }
+
+    @Override
     public List<Squad> getAllSquads() {
-        return squadRepository.findAll();
+
+        try {
+            return squadRepository.findAll();
+        }
+        catch (Exception e){
+            System.out.println("Erro ao encontrar objeto!");
+            return null;
+        }
     }
 
-    public Squad getSquadById(Long id) {
-        return squadRepository.findById(id).orElse(null);
+    @Override
+    public Optional<Squad> getSquadById(Long id) {
+
+        return squadRepository.findById(id);
     }
 
+    @Override
     public Squad saveSquad(Squad squad) {
+
         return squadRepository.save(squad);
     }
 
+    /*
+    public Squad createSquad(Squad squad) {
+        List<Student> students = squad.getStudents();
+        for (Student student : students) {
+            Optional<Student> optionalStudent = studentRepository.findById(student.getId());
+            if (optionalStudent.isEmpty()) {
+                throw new IllegalArgumentException("O aluno com ID " + student.getId() + " não foi encontrado.");
+            }
+        }
+
+        Squad newSquad = new Squad();
+        newSquad.setSquadName(squad.getSquadName());
+
+        // Associar os estudantes ao novo Squad por meio de seus IDs
+        List<Student> squadStudents = studentRepository.findAllById(students.stream().map(Student::getId).toList());
+        newSquad.setStudents(students);
+
+        // Salvar o novo Squad no banco de dados
+        return squadRepository.save(newSquad);
+    }
+
+     */
+
+    public Squad createSquad(Squad squad) {
+        List<Student> students = squad.getStudents();
+        for (Student student : students) {
+            Long studentId = student.getId();
+            if (!studentRepository.existsById(studentId)) {
+                throw new IllegalArgumentException("O aluno com ID " + studentId + " não foi encontrado.");
+            }
+        }
+
+        return squadRepository.save(squad);
+    }
+
+    @Override
     public void deleteSquad(Long id) {
+
         squadRepository.deleteById(id);
     }
 
