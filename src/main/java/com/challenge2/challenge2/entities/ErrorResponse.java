018d059e969d24ad978ec1c149b94720d1360768
Ualaci Anjos <ualaci.anjos.pb@compasso.com.br>
2023-07-29 11:21:09 -0300

    commit com as alterações da marina juntas

--- src/main/java/com/challenge2/challenge2/entities/ErrorResponse.java
+++ src/main/java/com/challenge2/challenge2/entities/ErrorResponse.java
@@ -1,29 +1,19 @@
 package com.challenge2.challenge2.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErrorResponse {
-
-    public ErrorResponse(){}
-    
-    public int status;
     public String message;
-    public long timeStamp;
-
-    public int getStatus() {
-        return status;
-    }
-    public void setStatus(int status) {
-        this.status = status;
-    }
-    public String getMessage() {
-        return message;
-    }
-    public void setMessage(String message) {
-        this.message = message;
-    }
-    public long getTimeStamp() {
-        return timeStamp;
-    }
-    public void setTimeStamp(long timeStamp) {
-        this.timeStamp = timeStamp;
-    }    
+    public Timestamp timestamp;
+    public String httpStatus;
 }
--- src/main/java/com/challenge2/challenge2/restControllers/controllersAdvices/ClassesRestExceptionHandler.java
+++ src/main/java/com/challenge2/challenge2/restControllers/controllersAdvices/ClassesRestExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.challenge2.challenge2.restControllers.controllersAdvices;
 
+import com.challenge2.challenge2.exceptions.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,26 +10,28 @@ import org.springframework.web.context.request.WebRequest;
 import com.challenge2.challenge2.entities.ErrorResponse;
 import com.challenge2.challenge2.exceptions.NotFoundException;
 
+import java.sql.Timestamp;
+
 @RestControllerAdvice
 public class ClassesRestExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException ex){
-        ErrorResponse error = new ErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(ex.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
+        ErrorResponse error = new ErrorResponse(ex.getMessage(), new Timestamp(System.currentTimeMillis()),HttpStatus.NOT_FOUND.name());
 
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> globlalExceptionHandler(Exception ex, WebRequest request) {
-        ErrorResponse error = new ErrorResponse();
-        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-        error.setMessage("Something went wrong!");
-        error.setTimeStamp(System.currentTimeMillis());
+        ErrorResponse error = new ErrorResponse("Something went wrong", new Timestamp(System.currentTimeMillis()),HttpStatus.NOT_FOUND.name());
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException ex){
+        ErrorResponse error = new ErrorResponse(ex.getMessage(), new Timestamp(System.currentTimeMillis()),HttpStatus.BAD_REQUEST.name());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
