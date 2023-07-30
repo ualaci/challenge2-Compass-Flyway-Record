c873913dbc6604ebc2aad40530a88829c11b5488
Marina Flores <mflores@ionic.health>
2023-07-30 10:08:35 -0300

    feat: incluindo documentação com swagger

---
+++ src/main/java/com/challenge2/challenge2/configuration/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.challenge2.challenge2.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.challenge2.challenge2")) 
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API Documentation")
+                .description("A Compass Scholarship Program classes registration API is designed to manage information related to classes offered by the Compass Scholarship Program.")
+                .version("1.0.0")
+                .build();
+    }
+}
